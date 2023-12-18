7a33338ffdfe21e99157f54858658f81ba435ae4
olgazh565 <olgazh565@gmail.com>
2023-12-19 00:08:09 +0300

    fixed redirect

--- src/api/const.js
+++ src/api/const.js
@@ -2,6 +2,6 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = '5qReNldMQm3nrcAJHVLN1w';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-deploy.vercel.app/auth';
 export const SCOPE = 'identity submit read';
 export const URL_API = 'https://oauth.reddit.com';
